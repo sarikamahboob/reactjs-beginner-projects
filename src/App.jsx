0e4cfb6cfac05a5aa6acd3ca3dbbb1953d2b687a
sarikamahboob <sarika.mahboob1234@gmail.com>
2025-05-22 00:48:56 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Counter from './Counter/Counter'
+import Todo from './Todo/Todo'
+
+
+function App() {
+
+  return (
+    <>
+      {/* <Counter /> */}
+      <Todo />
+    </>
+  )
+}
+
+export default App
